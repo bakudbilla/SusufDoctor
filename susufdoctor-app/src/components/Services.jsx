c79f073e8f9fda9660b13fc548f99e568f09efe0
Bernice <berniceawinpang@gmail.com>
2025-11-02 23:13:52 +0200

    image

--- susufdoctor-app/src/components/Services.jsx
+++ susufdoctor-app/src/components/Services.jsx
@@ -2,6 +2,8 @@ import { motion } from "framer-motion";
 import { SlideLeft } from "../utils/animations";
 import report from "../assets/report.png"
 import gradcam from "../assets/Gradcam.jpeg"
+import rlhf from "../assets/RLHF.png"
+import patient_analysis from  "../assets/patient_analysis.jpeg"
 
 export default function Services() {
   const services = [
@@ -11,17 +13,17 @@ export default function Services() {
       desc: "Automatically generate preliminary reports using standardized templates. Saves time, reduces errors, and enhances diagnostic accuracy."
     },
     {
-      img:{gradcam},
+      img:gradcam,
       title: "Gradcam Visualization",
       desc: "Instantly visualize the exact areas the AI focuses on within the image. Enhances diagnostic confidence and communication with clinicians."
     },
     {
-      img: "/src/assets/RLHF.png",
+      img: rlhf,
       title: "RLHF",
       desc: "Refines reports continuously using Human Feedback. Ensures the system evolves to meet expert radiologists’ standards."
     },
     {
-      img: "/src/assets/patient_analysis.jpeg",
+      img: patient_analysis,
       title: "Patient Analysis",
       desc: "Provides immediate patient-level insights. Supports clinicians in decision-making and timely interventions."
     },
