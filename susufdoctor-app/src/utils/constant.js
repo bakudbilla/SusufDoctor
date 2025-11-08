f29912286de7fee29b21b2f5db32c94c3ea5c637
Bernice <berniceawinpang@gmail.com>
2025-11-08 23:18:14 +0200

    src changed

--- susufdoctor-app/src/utils/constant.js
+++ susufdoctor-app/src/utils/constant.js
@@ -1,5 +1,5 @@
-// export const API_URL= 'https://susufdoctor-model-api.onrender.com/';
-export const API_URL= 'http://127.0.0.1:8000/';
+ export const API_URL= 'https://susufdoctor-model-api.onrender.com/';
+// export const API_URL= 'http://127.0.0.1:8000/';
 
 
 export const services = [
