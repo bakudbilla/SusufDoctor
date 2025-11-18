8220e05ebbcc7d5f852d069fbd434346bfd1d328
Bernice <berniceawinpang@gmail.com>
2025-11-18 12:31:11 +0200

    gcp deployment

--- fast_api/main.py
+++ fast_api/main.py
@@ -94,5 +94,5 @@ app.include_router(analytics_routes.router, tags=["Analytics"])
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
